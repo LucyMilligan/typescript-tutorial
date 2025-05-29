0d6f262ac508966d79b953ecc8e97028bfe03d90
Lucy Milligan <lucyjmilligan@yahoo.co.uk>
2025-05-29 15:32:21 +0100

    tutorial notes

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,51 @@
 "use strict";
+var _a;
 let age = 20;
 if (age < 50) {
     age += 10;
 }
 console.log(age);
+let numbers = [];
+let user = [1, "Lucy"];
+user.push(1);
+console.log(user);
+let mySize = 4;
+console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
+}
+let employee = {
+    id: 1,
+    name: "Lucy",
+    retire: (date) => console.log(date),
+};
+function kgToLbs(weight) {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+console.log(kgToLbs(10));
+console.log(kgToLbs("10kg"));
+let textBox = {
+    drag: () => { },
+    resize: () => { },
+};
+let quantity = 50;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log("Hola!");
+}
+greet(undefined);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,94 @@
+//Basics
 let age: number = 20;
 if (age < 50) {
   age += 10;
 }
 console.log(age);
+
+let numbers: number[] = [];
+
+let user: [number, string] = [1, "Lucy"];
+user.push(1);
+console.log(user);
+
+//enum
+const enum Size {
+  Small = 3,
+  Medium,
+  Large,
+}
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+//Functions
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 2022) {
+    return income * 1.2;
+  }
+  return income * 1.3;
+}
+
+//Type Aliases
+type Employee = {
+  readonly id: number;
+  name: string;
+  email?: string;
+  retire: (date: Date) => void;
+};
+
+let employee: Employee = {
+  id: 1,
+  name: "Lucy",
+  retire: (date: Date) => console.log(date),
+};
+
+//Union types
+function kgToLbs(weight: number | string): number {
+  //narrowing
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
+}
+
+console.log(kgToLbs(10));
+console.log(kgToLbs("10kg"));
+
+//Intersection types
+type Draggable = {
+  drag: () => void;
+};
+
+type Resizable = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
+
+//Literal Types
+type Quantity = 50 | 100;
+let quantity: Quantity = 50;
+
+//Nullable Types
+function greet(name: string | null | undefined) {
+  if (name) console.log(name.toUpperCase());
+  else console.log("Hola!");
+}
+
+greet(undefined);
+
+//Optional Chaining
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(1);
+console.log(customer?.birthday);
+console.log(customer?.birthday?.getFullYear());
